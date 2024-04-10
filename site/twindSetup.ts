5040750fee7124c7a1e4f3294c9f8d83d0b657a8
Juho Vepsäläinen <bebraw@gmail.com>
2024-04-10 14:00:48 +0300

    chore: Improve footer

--- site/twindSetup.ts
+++ site/twindSetup.ts
@@ -1,7 +1,18 @@
 import presetAutoprefix from "https://esm.sh/@twind/preset-autoprefix@1.0.5";
 import presetTailwind from "https://esm.sh/@twind/preset-tailwind@1.1.1";
 import presetTypography from "https://esm.sh/@twind/preset-typography@1.0.5";
+import meta from "./meta.json" with { type: "json" };
 
 export default {
   presets: [presetAutoprefix(), presetTailwind(), presetTypography()],
+  theme: {
+    extend: {
+      colors: meta.colors,
+      fontFamily: {
+        // TODO: Update this
+        // "primary": 'system-ui, "Eau"',
+        "monospace": "monospace",
+      },
+    },
+  },
 };
