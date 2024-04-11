ab195b4cafdddd7d7e349582b190a3d05ee17e2a
Juho Vepsäläinen <bebraw@gmail.com>
2024-04-11 09:13:16 +0300

    chore: Try linking deno

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.42.1
-/opt/buildhome/.deno/bin/deno task bootstrap
-/opt/buildhome/.deno/bin/deno task build
+
+ln -s /opt/buildhome/.deno/bin/deno deno
+
+deno task bootstrap
+deno task build
