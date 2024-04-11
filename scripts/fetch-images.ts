5b424bc5ca2653b8352e6b9fe941efe3e8da5a1f
Juho Vepsäläinen <bebraw@gmail.com>
2024-04-11 12:58:12 +0300

    chore: Simplify code

--- scripts/fetch-images.ts
+++ scripts/fetch-images.ts
@@ -1,16 +1,14 @@
 import { load } from "https://deno.land/std@0.221.0/dotenv/mod.ts";
 
-// TODO: Likely load() is checking env already so this code can be simplified
 const env = await load();
 
-const imagesEndpoint = env.GET_IMAGES_ENDPOINT ||
-  Deno.env.get("GET_IMAGES_ENDPOINT");
+const imagesEndpoint = env.GET_IMAGES_ENDPOINT;
 
 if (!imagesEndpoint) {
   throw new Error("Missing images endpoint from the environment!");
 }
 
-const imagesToken = env.GET_IMAGES_TOKEN || Deno.env.get("GET_IMAGES_TOKEN");
+const imagesToken = env.GET_IMAGES_TOKEN;
 
 if (!imagesToken) {
   throw new Error("Missing images token from the environment!");
