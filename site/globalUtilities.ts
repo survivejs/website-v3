525e30f76d9f99dd4654f93070021f5949b054a5
Juho Vepsäläinen <bebraw@gmail.com>
2024-04-11 10:47:10 +0300

    chore: Use correct layout

--- site/globalUtilities.ts
+++ site/globalUtilities.ts
@@ -1,9 +1,18 @@
-import { urlJoin } from "https://bundle.deno.dev/https://deno.land/x/url_join@1.0.0/mod.ts";
+import { urlJoin as urlJoinFn } from "https://bundle.deno.dev/https://deno.land/x/url_join@1.0.0/mod.ts";
 import type { Routes } from "https://deno.land/x/gustwind@v0.66.2/types.ts";
 
 function init({ routes }: { routes: Routes }) {
   function length(arr: unknown[]) {
     return arr.length;
+  }
+
+  function urlJoin(...parts: string[]) {
+    if (!parts.every((s) => typeof s === "string")) {
+      console.error(parts);
+      throw new Error("Failed to join url");
+    }
+
+    return urlJoinFn(...parts);
   }
 
   // Add your global page utilities here.
