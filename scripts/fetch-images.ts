bc5b2794fd6b091a8373134282a756adce12630f
Juho Vepsäläinen <bebraw@gmail.com>
2024-04-11 09:19:38 +0300

    chore: Add checks

--- scripts/fetch-images.ts
+++ scripts/fetch-images.ts
@@ -2,9 +2,21 @@ import { load } from "https://deno.land/std@0.221.0/dotenv/mod.ts";
 
 const env = await load();
 
-const images = Object.fromEntries((await fetch(env.GET_IMAGES_ENDPOINT, {
+const imagesEndpoint = env.GET_IMAGES_ENDPOINT;
+
+if (!imagesEndpoint) {
+  throw new Error("Missing images endpoint from the environment!");
+}
+
+const imagesToken = env.GET_IMAGES_TOKEN;
+
+if (!imagesToken) {
+  throw new Error("Missing images token from the environment!");
+}
+
+const images = Object.fromEntries((await fetch(imagesEndpoint, {
   headers: {
-    Authorization: `Bearer ${env.GET_IMAGES_TOKEN}`,
+    Authorization: `Bearer ${imagesToken}`,
   },
 }).then((res) => res.json()).catch((err) => console.error(err)) as {
   filename: string;
