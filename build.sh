4f2d9c835d58d13c83fd2d3992f994ae528bfd50
Juho Vepsäläinen <bebraw@gmail.com>
2024-04-11 09:42:51 +0300

    chore: Echo sha

--- build.sh
+++ build.sh
@@ -7,5 +7,7 @@ curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.42.1
 export DENO_INSTALL="/opt/buildhome/.deno"
 export PATH="$DENO_INSTALL/bin:$PATH"
 
+echo $CF_PAGES_COMMIT_SHA
+
 deno task bootstrap
 deno task build
