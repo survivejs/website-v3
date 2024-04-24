c01676261e9e2f4d0d2153618d30d87883c44fb1
Juho Vepsäläinen <bebraw@gmail.com>
2024-04-24 13:41:17 +0300

    chore: Disable CF hack for now

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.42.1
 
 # Trick Cloudflare into caching Deno dependencies by leveraging a convention
 # https://developers.cloudflare.com/pages/configuration/build-caching/#frameworks
-export DENO_DIR="/opt/buildhome/.cache"
+# export DENO_DIR="/opt/buildhome/.cache"
 
 export DENO_INSTALL="/opt/buildhome/.deno"
 export PATH="$DENO_INSTALL/bin:$PATH"
