0600409bc67271ab6b2f7d6b5ed542a10a11d0c5
Juho Vepsäläinen <bebraw@gmail.com>
2024-04-24 16:56:23 +0300

    fix: Pass missing parameters

--- site/components/Markdown.server.ts
+++ site/components/Markdown.server.ts
@@ -1,8 +1,8 @@
 import getMarkdown from "../transforms/markdown.ts";
-import type { LoadApi } from "https://deno.land/x/gustwind@v0.66.2/types.ts";
+import type { DataSourcesApi } from "https://deno.land/x/gustwind@v0.70.3/types.ts";
 
-function init({ load }: { load: LoadApi }) {
-  const markdown = getMarkdown(load);
+function init({ load, render }: DataSourcesApi) {
+  const markdown = getMarkdown({ load, render });
 
   return {
     processMarkdown: async (input: string) => (await markdown(input)).content,
