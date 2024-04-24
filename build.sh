42828d6d1ad6b6e998a93ffac382d2fef1d5eb11
Juho Vepsäläinen <bebraw@gmail.com>
2024-04-24 14:05:35 +0300

    chore: Drop Deno caching experiment

--- build.sh
+++ build.sh
@@ -4,11 +4,7 @@ set -e # Exit with nonzero exit code if anything fails
 
 curl -fsSL https://deno.land/x/install/install.sh | sh -s v1.42.1
 
-# Trick Cloudflare into caching Deno dependencies by leveraging a convention
-# https://developers.cloudflare.com/pages/configuration/build-caching/#frameworks
-# export DENO_DIR="/opt/buildhome/.cache"
-
-export DENO_INSTALL="/opt/buildhome/.cache"
+export DENO_INSTALL="/opt/buildhome/.deno"
 export PATH="$DENO_INSTALL/bin:$PATH"
 
 deno task bootstrap
