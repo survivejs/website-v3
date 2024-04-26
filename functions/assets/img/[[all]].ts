01f499f43bdb4f3dbf82c6d7c969c57581cc2d40
Juho Vepsäläinen <bebraw@gmail.com>
2024-04-26 16:41:52 +0300

    chore: Simplify image loading logic

--- functions/assets/img/[[all]].ts
+++ functions/assets/img/[[all]].ts
@@ -1,7 +1,6 @@
 import images from "../../../.images.json" assert { type: "json" };
 import { type Env } from "../../../types.ts";
 
-// TODO: Set up a proxy so that gustwind can forward assuming it's running in front
 // TODO: Figure out how to handle book images. I.e., images/babel.png
 // Likely the images have to be pushed to the origin separately somehow
 // and then they need to be resolved here (needs different route but same logic)
@@ -28,7 +27,8 @@ export function onRequest(
   }
 
   const type = "public"; // "public" or "thumb"
-  const url = `${context.env.IMAGES_API_URL}?image=${imageId}&type=${type}`;
+  // The assumption here is that IMAGES_API_URL ends with a slash
+  const url = `${context.env.IMAGES_API_URL}${imageId}/${type}`;
 
   return fetch(url);
 }
