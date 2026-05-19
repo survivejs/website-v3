b793595b9353e90e9ceb3c32a12642b281c511a8
Juho Vepsäläinen <bebraw@gmail.com>
2026-05-19 10:58:47 +0300

    Update Gustwind to 0.112.0

--- scripts/build-site.mjs
+++ scripts/build-site.mjs
@@ -3,22 +3,11 @@ import { asNodePluginDefinitions } from "../site/gustwind.config.mjs";
 
 const outputDirectory = "./build";
 
-try {
-  await buildNode({
-    cwd: process.cwd(),
-    outputDirectory,
-    pluginDefinitions: asNodePluginDefinitions(),
-    validateOutput: process.argv.includes("--validate"),
-    incremental: !process.argv.includes("--no-incremental"),
-    cacheFrom: outputDirectory,
-  });
-} catch (error) {
-  if (
-    error instanceof TypeError &&
-    error.message.includes(".stop is not a function")
-  ) {
-    console.warn("gustwind: ignored esbuild cleanup compatibility warning");
-  } else {
-    throw error;
-  }
-}
+await buildNode({
+  cwd: process.cwd(),
+  outputDirectory,
+  pluginDefinitions: asNodePluginDefinitions(),
+  validateOutput: process.argv.includes("--validate"),
+  incremental: !process.argv.includes("--no-incremental"),
+  cacheFrom: outputDirectory,
+});
